c4727723d23126ca2ac31fdd46c3d828bc64d41d
shuhan-duan <114911788+shuhan-duan@users.noreply.github.com>
2023-09-19 11:05:20 +0200

    Add files via upload

---
+++ DAAR/src/tme1/NDFA.java
@@ -0,0 +1,11 @@
+package tme1;
+
+public class NDFA {
+	//MACROS
+	
+
+	public NDFA() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ DAAR/src/tme1/State.java
@@ -0,0 +1,14 @@
+package tme1;
+
+import java.util.HashMap;
+
+public class State {
+	private  boolean isFinal;
+	private  
+	
+
+	public State() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
